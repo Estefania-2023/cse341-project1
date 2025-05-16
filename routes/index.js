733fcfe6d1d81aac6b355bfbae61e4d6e6834574
Estefania-2023 <estefa20or@gmail.com>
2025-05-16 11:23:35 -0500

    installing swagger week2's assignment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => { res.send('Welcome to the home page!');});
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => { 
+    //#swagger.tags = ['Hello world!']
+    res.send('Hello world!');});
 
 router.use('/contacts', require('./contacts'));
 
